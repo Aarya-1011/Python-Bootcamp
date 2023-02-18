d2c9827bb02f9e85f013eeaf76a3670c17d64671
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-02-18 11:37:26 +0530

    Create Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,6 @@
+#Write a python program to allow the user to enter a name, a place and a number and generate the output.
+
+a=str(input())
+b=str(input())
+c=int(input())
+print(a+ " went to "+b+ " and bought "+str(c)+ " apples")
