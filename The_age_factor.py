56e24d6905b0ca8cab3040a88715d18919430a30
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-02-18 11:43:14 +0530

    Create The_age_factor.py

---
+++ The_age_factor.py
@@ -0,0 +1,6 @@
+#Write a python program to calculate the difference between the ages of 2 people.
+
+n1=int(input())
+n2=int(input())
+n3=n1-n2
+print("The difference between their age is " + str(n3))
