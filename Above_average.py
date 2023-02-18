255faca31b2aa75fe1a1f827fd8f2a76590c4a6e
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-02-18 12:08:46 +0530

    Create Above_average.py

---
+++ Above_average.py
@@ -0,0 +1,13 @@
+#You are given a list of 10 numbers. You have to print out the number of above average numbers in the list. Above average numbers are the numbers which are greater than the average of the numbers in the list.
+
+a=list(map(int,input().split()))
+avg=0
+sum=0
+for i in a:
+    sum=sum+i
+avg=sum/10
+b=[]
+for i in a:
+    if i > avg:
+        b.append(i)
+print(len(b))
